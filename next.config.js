dd41424aa5e65d6f843b7e1624f79d8ae0d4bf3e
unknown <limteikyee@gmail.com>
2019-02-06 18:45:13 +0800

    add bundker analyzer

--- next.config.js
+++ next.config.js
@@ -1,42 +1,59 @@
 const withFonts = require('next-fonts')
+const withBundleAnalyzer = require('@zeit/next-bundle-analyzer')
 
-module.exports = withFonts({
-    // webpack(config) {
-    //     config.module.rules.push(
-    //         // {
-    //         //     // parse css font face
-    //         //     test: /\.(png|woff|woff2|eot|svg)$/,
-    //         //     loader: 'url-loader?limit=100000',
-    //         // },
-    //         // {
-    //         //     test: /\.(png|jpg|gif)$/i,
-    //         //     use: [
-    //         //         {
-    //         //             loader: 'url-loader',
-    //         //             options: {
-    //         //                 limit: 8192,
-    //         //             },
-    //         //         },
-    //         //     ],
-    //         // },
-    //         {
-    //             // load ttf
-    //             test: /\.ttf$/,
-    //             use: [
-    //                 {
-    //                     loader: 'ttf-loader',
-    //                     options: {
-    //                         name: './font/[hash].[ext]',
-    //                     },
-    //                 },
-    //             ],
-    //         }
-    //     )
-    //     return config
-    // },
-    exportPathMap: function() {
-        return {
-            '/': { page: '/' },
-        }
-    },
-})
+module.exports = withBundleAnalyzer(
+    withFonts({
+        // webpack(config) {
+        //     config.module.rules.push(
+        //         // {
+        //         //     // parse css font face
+        //         //     test: /\.(png|woff|woff2|eot|svg)$/,
+        //         //     loader: 'url-loader?limit=100000',
+        //         // },
+        //         // {
+        //         //     test: /\.(png|jpg|gif)$/i,
+        //         //     use: [
+        //         //         {
+        //         //             loader: 'url-loader',
+        //         //             options: {
+        //         //                 limit: 8192,
+        //         //             },
+        //         //         },
+        //         //     ],
+        //         // },
+        //         {
+        //             // load ttf
+        //             test: /\.ttf$/,
+        //             use: [
+        //                 {
+        //                     loader: 'ttf-loader',
+        //                     options: {
+        //                         name: './font/[hash].[ext]',
+        //                     },
+        //                 },
+        //             ],
+        //         }
+        //     )
+        //     return config
+        // },
+        analyzeServer: ['server', 'both'].includes(process.env.BUNDLE_ANALYZE),
+        analyzeBrowser: ['browser', 'both'].includes(
+            process.env.BUNDLE_ANALYZE
+        ),
+        bundleAnalyzerConfig: {
+            server: {
+                analyzerMode: 'static',
+                reportFilename: '../bundles/server.html',
+            },
+            browser: {
+                analyzerMode: 'static',
+                reportFilename: '../bundles/client.html',
+            },
+        },
+        exportPathMap: function() {
+            return {
+                '/': { page: '/' },
+            }
+        },
+    })
+)
